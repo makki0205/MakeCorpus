aa8759314fe545c3bff9f7dcd47520217ea862a5
makki0205 <llxo2_5oxll@icloud.com>
2016-07-06 17:54:46 +0900

    プルリク

--- models/users.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var DataSchema = new Schema({
-    talkId : { type: Number,},
-    sentence: { type: String },
-    sentenceId: { type: Number }
-});
-module.exports = mongoose.model('Data', DataSchema);
